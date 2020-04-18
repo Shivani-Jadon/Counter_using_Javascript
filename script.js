eba6439e21a74ef9ac5a9ddf6e9005074ed46728
Shivani <shivanijadon98@gmail.com>
2020-04-18 18:48:36 +0530

    add reset button

--- script.js
+++ script.js
@@ -1,31 +1,23 @@
-/*function light(count){
+var interval; 
 
-    let  msg = document.getElementById("ctr2");
-    msg.style.display = "flex";
-    msg.innerHTML = count;
-    count--;
-}
-
-function dark(){
-    let  msg = document.getElementById("ctr2");
-    msg.style.display = "none";
-}
-*/
+//function for starting counter
 function counter_control(){
 
     let ctr = document.getElementById("data").value;
+    let msg = document.getElementById("ctr2");
+    document.getElementById("ctr1").style.display = "flex";
+    document.getElementById("invalid").style.display = "none";
 
     ctr = parseInt(ctr);
     console.log(ctr);
 
+    //starting counter for input 0-9
     if(ctr>=0 && ctr<=9)
     {
         let count = ctr;  
         
-        let interval = setInterval(function(){
-
-            let msg = document.getElementById("ctr2");
-            
+        interval = setInterval(function(){
+            //stopping counter 
             if(count < 0)
             {
                 msg.style.display = "none";
@@ -33,25 +25,46 @@ function counter_control(){
                 clearInterval(interval);              
                 return;                
             }
-        
-            if( msg.style.display == "none"){
+
+            //making counter visible
+            if( msg.style.display == "none"){   
                 msg.style.display = "flex";
                 msg.innerHTML = count;
                 count--;
-            }else if(msg.style.display != "none"){
+            }
+            //making counter invisible
+            else if(msg.style.display != "none"){
                 msg.style.display = "none";
             }
             
             
         }, ctr*100);
        
-       // window.alert("counter starts");   
-    }else{
-        document.getElementById("counter").innerText = "Invalid Input"; 
+    }
+    //invalid msg on other inputs
+    else{
+        document.getElementById("ctr1").style.display = "none"; 
+        document.getElementById("invalid").style.display = "flex"; 
+        return;
     };
 
 }
 
-var button = document.getElementById("btn");
+function reset_counter(){
 
-button.addEventListener("click", counter_control);
+    document.getElementById("ctr1").style.display = "flex";
+    document.getElementById("invalid").style.display = "none";
+    
+    let msg = document.getElementById("ctr2");
+    msg.style.display = "none";
+    document.getElementById("data").innerHTML = "0";
+    clearInterval(interval);              
+    return; 
+}
+
+var button1 = document.getElementById("btn1");
+var button2 = document.getElementById("btn2");
+
+button1.addEventListener("click", counter_control);
+
+button2.addEventListener("click", reset_counter);
